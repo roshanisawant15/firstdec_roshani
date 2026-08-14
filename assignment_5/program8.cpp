008ce6754ed34fca7c7b4ea09994eedb73b95d50
roshanisawant15 <tinusawant.1234@gmail.com>
2026-08-14 11:21:19 +0530

    Add files via upload
    Pattern prgram

---
+++ assignment_5/program8.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    // Increasing pattern
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    // Decreasing pattern
+    for(i = n - 1; i >= 1; i--)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
